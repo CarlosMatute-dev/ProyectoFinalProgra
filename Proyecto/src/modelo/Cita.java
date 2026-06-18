e34d13b7fec06a655b37d01365430f4ee43ad1e0
CarlosMatute-dev <carlo.jesum06@gmail.com>
2026-06-18 17:56:36 -0400

    evritin

--- Proyecto/src/modelo/Cita.java
+++ Proyecto/src/modelo/Cita.java
@@ -1,4 +1,71 @@
 package modelo;
+
+import java.time.LocalDateTime;
+
 public class Cita {
-    //aa
+    private int idCita;
+    private LocalDateTime fechaHora;
+    private Cliente cliente;
+    private Diseño diseno;
+    private EstadoCita estado;
+    private double precio;
+
+    public Cita(int idCita, LocalDateTime fechaHora, Cliente cliente, Diseño diseno, EstadoCita estado, double precio) {
+        this.idCita = idCita;
+        this.fechaHora = fechaHora;
+        this.cliente = cliente;
+        this.diseno = diseno;
+        this.estado = estado;
+        this.precio = precio;
+    }
+
+    public int getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(int idCita) {
+        this.idCita = idCita;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Diseño getDiseno() {
+        return diseno;
+    }
+
+    public void setDiseno(Diseño diseno) {
+        this.diseno = diseno;
+    }
+
+    public EstadoCita getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCita estado) {
+        this.estado = estado;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
 }
