c615a7dd79a8373fd0e1ae74176e3ea4f25e3357
Seiyacx <carloscubillangutierrez@gmail.com>
2026-06-25 18:19:33 -0400

    fix2

--- Proyecto/src/modelo/Cita.java
+++ Proyecto/src/modelo/Cita.java
@@ -6,21 +6,6 @@ import java.time.LocalDateTime;
 public class Cita {
     private static int contador;
     private int idCita;
-<<<<<<< HEAD
-    private LocalDateTime fechaHora;
-    private Cliente cliente;
-    private Diseño diseno;
-    private Tatuador tatuador;
-    private EstadoCita estado;
-    private double precio;
-
-    public Cita(int idCita, LocalDateTime fechaHora, Cliente cliente, Diseño diseno, Tatuador tatuador, EstadoCita estado, double precio) {
-        this.idCita = idCita;
-        this.fechaHora = fechaHora;
-        this.cliente = cliente;
-        this.diseno = diseno;
-        this.tatuador = tatuador;
-=======
     private LocalDate fecha;
     private int idCliente;
     private int idDiseno;
@@ -35,20 +20,9 @@ public class Cita {
         this.fecha = fecha;
         this.idCliente = idCliente;
         this.idDiseno = idDiseno;
->>>>>>> eb1f5e2ed0f7bc0ecd871b6699c877dfb8ca3e76
         this.estado = estado;
         this.precio = precio;
-    }
-
-    public Tatuador getTatuador() {
-        return tatuador;
-    }
-
-    public void setTatuador(Tatuador tatuador) {
-        this.tatuador = tatuador;
-    }
-
-    
+    }    
 
     public int getIdCita() {
         return idCita;
